7030506d5b2073a454b916f402ae64d6e597d14b
Sepehr Mohammadi <mohamadi.sepehr@live.com>
2021-09-06 17:59:45 +0430

    added support for clusters

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/go-redsync/redsync/v4"
-	"github.com/go-redsync/redsync/v4/redis/redigo"
-	"github.com/gomodule/redigo/redis"
+	redsync_goredis "github.com/go-redsync/redsync/v4/redis/goredis/v8"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -20,7 +19,7 @@ var (
 type Config struct {
 	Redis struct {
 		Host     string
-		Port     int
+		RoHost   string // readonly host
 		Password string
 		Database int
 	}
@@ -43,20 +42,43 @@ func init() {
 
 func main() {
 
-	// Create a pool of redis connections.
-	pool := redigo.NewPool(&redis.Pool{
-		MaxIdle:     3,
-		IdleTimeout: 240 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", fmt.Sprintf("%s:%d", config.Redis.Host, config.Redis.Port), redis.DialPassword(config.Redis.Password), redis.DialDatabase(config.Redis.Database))
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			return err
+	// clusterSlots returns cluster slots information.
+	clusterSlots := func(context.Context) ([]redis.ClusterSlot, error) {
+		slots := []redis.ClusterSlot{
+			// 1 master and 1 slave.
+			{
+				Start: 0,
+				End:   16383,
+				Nodes: []redis.ClusterNode{{
+					Addr: config.Redis.Host, // master
+				}, {
+					Addr: config.Redis.RoHost, // 1st slave
+				}},
+			},
+		}
+		return slots, nil
+	}
+
+	cl := redis.NewClusterClient(&redis.ClusterOptions{
+		Password:     config.Redis.Password,
+		ReadOnly:     true,
+		ClusterSlots: clusterSlots,
+		NewClient: func(opt *redis.Options) *redis.Client {
+			opt.DB = config.Redis.Database
+			client := redis.NewClient(opt)
+			return client
 		},
 	})
 
-	rs := redsync.New(pool)
+	defer cl.Close()
+
+	err := cl.Ping(context.Background()).Err()
+	if err != nil {
+		logrus.WithError(err).Fatal("Error connecting to redis")
+	}
+
+	cl.ReloadState(context.Background())
+	rs := redsync.New(redsync_goredis.NewPool(cl))
 
 	mutex := rs.NewMutex("output", redsync.WithExpiry(time.Second*5))
 	logrus.Info("Waiting")
